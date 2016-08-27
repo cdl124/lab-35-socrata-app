fd665dbde4dbeee9e22bd7ef6b64d8e18841c246
Cortney Lane <cdlane124@gmail.com>
2016-08-27 11:19:08 -0700

    filled out webpack.config, installed packages, created .babelrc, started entry.js

---
+++ lab-cortney/webpack.config.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractText = require('extract-text-webpack-plugin');
+
+const production = process.env.NODE_ENC === 'production';
+const apiURL = process.env.API_URL || 'http://localhost:3000';
+
+var plugins = [
+  new ExtractText('bundle.css'),
+  new webpack.DefinePlugin({
+    __API_URL__: JSON.stringify(apiURL)
+  })
+];
+
+if (production) {
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new CleanPlugin()
+  ]);
+}
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  debug: !production,
+  devTool: production ? false : 'eval',
+  plugins: plugins,
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss/lib`]
+  },
+  postcss: function(){
+    return [autoprefixer];
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractText.extract('style', 'css!postcss!sass')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        loader: 'file?name=img/[hash].[ext]'
+      }
+    ]
+  }
+};
